dd820fdf787714258f292e54964151ce64c198b8
Adam Barák <barak.adam94@gmail.com>
2017-09-03 18:07:06 +0200

    BARA-001: Initial branch - Constants

---
+++ src/util/Constants.java
@@ -0,0 +1,18 @@
+package util;
+
+/**
+ * Final class with constants used across project
+ * 
+ * @author Adam Barák
+ */
+public final class Constants {
+
+	/**
+	 * Final class with error codes used across project
+	 * 
+	 * @author Adam Barák
+	 */
+	public final class ErrorCodes {
+		
+	}
+}
